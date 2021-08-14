422fe07ad222994988d865c660f928efb3763ee4
nugi-developers <68599801+Erehmaryann@users.noreply.github.com>
2021-08-13 19:52:50 -0700

    declared a var for the media stream api

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const button = document.getElementById("button");
 // Prompt a user to select media stream, pass to the video element, then play.
 async function selectMediaStream() {
   try {
+    //To get live stream
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
   } catch (e) {
     //Catch Error here
   }
