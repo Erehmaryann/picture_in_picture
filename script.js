e7a8e5b67e08cbbb6c22737c8cc54c89fec30cd5
nugi-developers <68599801+Erehmaryann@users.noreply.github.com>
2021-08-13 19:46:11 -0700

    called the media stream func on load

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ const button = document.getElementById("button");
 // Prompt a user to select media stream, pass to the video element, then play.
 async function selectMediaStream() {
   try {
-  } catch (e) {}
+  } catch (e) {
+    //Catch Error here
+  }
 }
+
+// On load
+selectMediaStream();
