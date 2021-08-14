8dadb39ebdfddd7ad7e2618aba65d5125adfa044
nugi-developers <68599801+Erehmaryann@users.noreply.github.com>
2021-08-13 19:38:53 -0700

    declared an async function for the media stream

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 const videoElement = document.getElementById("video");
 const button = document.getElementById("button");
+
+// Prompt a user to select media stream, pass to the video element, then play.
+async function selectMediaStream() {}
