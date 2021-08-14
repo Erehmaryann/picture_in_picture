68acac2641110958b25999333fd0adcbb0185c2f
nugi-developers <68599801+Erehmaryann@users.noreply.github.com>
2021-08-13 19:42:30 -0700

    added a try and catch block to the media stream func

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ const videoElement = document.getElementById("video");
 const button = document.getElementById("button");
 
 // Prompt a user to select media stream, pass to the video element, then play.
-async function selectMediaStream() {}
+async function selectMediaStream() {
+  try {
+  } catch (e) {}
+}
